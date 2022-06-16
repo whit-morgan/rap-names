b73838783da1b2d67db8c1c3f3b6e7ee1f22ad91
Whit Morgan <webdevwhit@gmail.com>
2022-06-16 16:01:35 -0400

    updated main.js to append mongodb data to dom

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 const deleteText = document.querySelectorAll('.fa-trash')
 const thumbText = document.querySelectorAll('.fa-thumbs-up')
+const moreInfoButton = document.querySelectorAll('.moreInfo')
 
 Array.from(deleteText).forEach((element)=>{
     element.addEventListener('click', deleteRapper)
@@ -7,6 +8,10 @@ Array.from(deleteText).forEach((element)=>{
 
 Array.from(thumbText).forEach((element)=>{
     element.addEventListener('click', addLike)
+})
+
+Array.from(moreInfoButton).forEach((element)=>{
+    element.addEventListener('click', moreInfo)
 })
 
 async function deleteRapper(){
@@ -33,7 +38,9 @@ async function deleteRapper(){
 async function addLike(){
     const sName = this.parentNode.childNodes[1].innerText
     const bName = this.parentNode.childNodes[3].innerText
-    const tLikes = Number(this.parentNode.childNodes[5].innerText)
+    const age = this.parentNode.childNodes[5].innerText
+    const origin = this.parentNode.childNodes[7].innerText
+    const tLikes = Number(this.parentNode.childNodes[9].innerText)
     try{
         const response = await fetch('addOneLike', {
             method: 'put',
@@ -41,6 +48,8 @@ async function addLike(){
             body: JSON.stringify({
               'stageNameS': sName,
               'birthNameS': bName,
+              'ageS': age,
+              'originS': origin,
               'likesS': tLikes
             })
           })
@@ -51,4 +60,34 @@ async function addLike(){
     }catch(err){
         console.log(err)
     }
+}
+
+
+function test(){
+    const x = document.getElementById('#test')
+    const y = x.document.getElementsByTagName('li')
+
+    console.log(y[0])
+}
+async function moreInfo(){
+    const sName= this.parentNode.childNodes[1].innerText
+    const moreInfoList = document.querySelector('#moreInfo')
+    try{
+        const response = await fetch(`api/${sName}`)
+        console.log(sName)
+
+        const data = await response.json()
+        console.log(data)
+
+
+        data.forEach(obj => {
+            const li = document.createElement('li')
+            console.log(obj)
+            li.textContent = `Birth Name: ${obj.birthName}`
+
+            moreInfoList.appendChild(li)
+        })
+    }catch(error){
+        console.error(error)
+    }   
 }
--- server.js
+++ server.js
@@ -22,16 +22,26 @@ app.use(express.json())
 
 
 app.get('/',(request, response)=>{
-    db.collection('rappers').find().sort({likes: -1}).toArray()
+    db.collection('rappers').find().toArray()
     .then(data => {
         response.render('index.ejs', { info: data })
+        console.log(data)
+    })
+    .catch(error => console.error(error))
+})
+
+app.get('/api/:stageName',(request, response)=>{
+    db.collection('rappers').find().toArray()
+    .then(data => {       
+        response.json(data)
+        console.log(data)
     })
     .catch(error => console.error(error))
 })
 
 app.post('/addRapper', (request, response) => {
     db.collection('rappers').insertOne({stageName: request.body.stageName,
-    birthName: request.body.birthName, likes: 0})
+    birthName: request.body.birthName, age: request.body.age, origin: request.body.origin, likes: 0})
     .then(result => {
         console.log('Rapper Added')
         response.redirect('/')
@@ -40,14 +50,16 @@ app.post('/addRapper', (request, response) => {
 })
 
 app.put('/addOneLike', (request, response) => {
-    db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS,likes: request.body.likesS},{
+    db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS, age: request.body.ageS, origin: request.body.originS, likes: request.body.likesS},{
         $set: {
             likes:request.body.likesS + 1
           }
-    },{
-        sort: {_id: -1},
-        upsert: true
-    })
+    },
+    // {
+    //     sort: {_id: -1},
+    //     upsert: true
+    // }
+    )
     .then(result => {
         console.log('Added One Like')
         response.json('Like Added')
